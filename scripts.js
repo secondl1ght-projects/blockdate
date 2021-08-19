6a819d480a191396f6edba91eab0ac2e6f21a016
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-08-19 12:12:06 -0600

    Format the easter eggs a bit more

--- scripts.js
+++ scripts.js
@@ -83,7 +83,7 @@ submitButton.onclick = async function handleOnClick() {
     const timestamp = new Date('2009-10-12 04:21:18 utc');
       document.querySelector('#button').innerHTML = 'GENERATE DATE';
       document.querySelector("#blockdate").innerHTML = timestamp;
-      document.querySelector('#easteregg').innerHTML = 'In this first-known BTC-to-fiat transaction, 5,050 BTC were exchanged for 5.02 USD, at an effective exchange rate of ~0.001 USD/BTC.<br><a href="https://nitter.pussthecat.org/marttimalmi/status/423455561703624704" target="_blank" rel="noreferrer">Twitter thread</a>';
+      document.querySelector('#easteregg').innerHTML = 'In this first-known BTC-to-fiat transaction, 5,050 BTC were exchanged for 5.02 USD, at an effective exchange rate of ~0.001 USD/BTC.<br><br><a href="https://nitter.pussthecat.org/marttimalmi/status/423455561703624704" target="_blank" rel="noreferrer">Twitter thread</a>';
       submitButton.disabled = false;
   }
 
@@ -91,7 +91,7 @@ submitButton.onclick = async function handleOnClick() {
     const timestamp = new Date('2010-05-22 18:16:31 utc');
       document.querySelector('#button').innerHTML = 'GENERATE DATE';
       document.querySelector("#blockdate").innerHTML = timestamp;
-      document.querySelector('#easteregg').innerHTML = '<i class="fas fa-pizza-slice"></i><br>This is the famous "Bitcoin Pizza" transaction, one of the earliest "real-world" transactions, in which 10,000 BTC was paid for 2 large pizzas worth ~41 USD (at an effective exchange rate of ~$0.04/BTC).<br><a href="https://bitcointalk.org/index.php?topic=137.0" target="_blank" rel="noreferrer">bitcointalk.org thread</a>';
+      document.querySelector('#easteregg').innerHTML = '<i class="fas fa-pizza-slice"></i><br>This is the famous `Bitcoin Pizza` transaction, one of the earliest `real-world` transactions, in which 10,000 BTC was paid for 2 large pizzas worth ~41 USD (at an effective exchange rate of ~$0.04/BTC).<br><br><a href="https://bitcointalk.org/index.php?topic=137.0" target="_blank" rel="noreferrer">bitcointalk.org thread</a>';
       submitButton.disabled = false;
   }
 
@@ -99,7 +99,7 @@ submitButton.onclick = async function handleOnClick() {
     const timestamp = new Date('2011-11-16 05:59:08 utc');
       document.querySelector('#button').innerHTML = 'GENERATE DATE';
       document.querySelector("#blockdate").innerHTML = timestamp;
-      document.querySelector('#easteregg').innerHTML = 'This transaction from November 2011 spends 11 UTXOs worth 50,000 BTC to create a single 550,000 BTC UTXO. This is currently the largest output transaction.<br><a href="https://mempool.space/tx/29a3efd3ef04f9153d47a990bd7b048a4b2d213daaa5fb8ed670fb85f13bdbcf" target="_blank" rel="noreferrer">View transaction</a>';
+      document.querySelector('#easteregg').innerHTML = 'This transaction from November 2011 spends 11 UTXOs worth 50,000 BTC to create a single 550,000 BTC UTXO. This is currently the largest output transaction.<br><br><a href="https://mempool.space/tx/29a3efd3ef04f9153d47a990bd7b048a4b2d213daaa5fb8ed670fb85f13bdbcf" target="_blank" rel="noreferrer">View transaction</a>';
       submitButton.disabled = false;
   }
 
@@ -107,7 +107,7 @@ submitButton.onclick = async function handleOnClick() {
     const timestamp = new Date('2012-11-28 15:24:38 utc');
       document.querySelector('#button').innerHTML = 'GENERATE DATE';
       document.querySelector("#blockdate").innerHTML = timestamp;
-      document.querySelector('#easteregg').innerHTML = 'First block of subsidy (miners reward) era #2 (25 BTC).';
+      document.querySelector('#easteregg').innerHTML = 'First block of subsidy (miner reward) era #2 (25 BTC).';
       submitButton.disabled = false;
   }
 
@@ -123,7 +123,7 @@ submitButton.onclick = async function handleOnClick() {
     const timestamp = new Date('2016-07-09 16:46:13 utc');
       document.querySelector('#button').innerHTML = 'GENERATE DATE';
       document.querySelector("#blockdate").innerHTML = timestamp;
-      document.querySelector('#easteregg').innerHTML = 'First block of subsidy (miners reward) era #3 (12.5 BTC).';
+      document.querySelector('#easteregg').innerHTML = 'First block of subsidy (miner reward) era #3 (12.5 BTC).';
       submitButton.disabled = false;
   }
 
@@ -131,7 +131,7 @@ submitButton.onclick = async function handleOnClick() {
     const timestamp = new Date('2020-03-11 22:57:21 utc');
       document.querySelector('#button').innerHTML = 'GENERATE DATE';
       document.querySelector("#blockdate").innerHTML = timestamp;
-      document.querySelector('#easteregg').innerHTML = '500+ million USD transferred for < 1 USD fee (2020 prices).<br><a href="https://mempool.space/tx/eeea72f5c9fe07178013eac84c3705443321d5453befd7591f52d22ac39b3963" target="_blank" rel="noreferrer">View transaction</a>';
+      document.querySelector('#easteregg').innerHTML = '500+ million USD transferred for < 1 USD fee (2020 prices).<br><br><a href="https://mempool.space/tx/eeea72f5c9fe07178013eac84c3705443321d5453befd7591f52d22ac39b3963" target="_blank" rel="noreferrer">View transaction</a>';
       submitButton.disabled = false;
   }
 
@@ -139,7 +139,7 @@ submitButton.onclick = async function handleOnClick() {
     const timestamp = new Date('2020-05-11 19:23:23 utc');
       document.querySelector('#button').innerHTML = 'GENERATE DATE';
       document.querySelector("#blockdate").innerHTML = timestamp;
-      document.querySelector('#easteregg').innerHTML = 'With the coinbase message "Times 09/Apr/2020 With $2.3T Injection, Fed`s Plan Far Exceeds 2008 Rescue", this final block of subsidy era #3 (12.5 BTC) echoes the spiritual call-to-action of Satoshi`s genesis block.';
+      document.querySelector('#easteregg').innerHTML = 'With the coinbase message `Times 09/Apr/2020 With $2.3T Injection, Fed`s Plan Far Exceeds 2008 Rescue`, this final block of subsidy era #3 (12.5 BTC) echoes the spiritual call-to-action of Satoshi`s genesis block.';
       submitButton.disabled = false;
   }
 
@@ -147,7 +147,7 @@ submitButton.onclick = async function handleOnClick() {
     const timestamp = new Date('2020-05-11 19:23:43 utc');
       document.querySelector('#button').innerHTML = 'GENERATE DATE';
       document.querySelector("#blockdate").innerHTML = timestamp;
-      document.querySelector('#easteregg').innerHTML = 'First block of subsidy (miners reward) era #4 (6.25 BTC).';
+      document.querySelector('#easteregg').innerHTML = 'First block of subsidy (miner reward) era #4 (6.25 BTC).';
       submitButton.disabled = false;
   }
 
